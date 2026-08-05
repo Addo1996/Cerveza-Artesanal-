215e16adf117fbcd6a322cd7aa0063f92be69801
Addo1996 <addo-fran@hotmail.es>
2026-08-04 19:46:13 -0500

    Se agrega codigo y se actualiza la logica del tanque

--- src/com/krakedev/artesanal/testJUnit/TestRecargarJUnit.java
+++ src/com/krakedev/artesanal/testJUnit/TestRecargarJUnit.java
@@ -1,36 +1,36 @@
 package com.krakedev.artesanal.testJUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 import com.krakedev.artesanal.Maquina;
+
 public class TestRecargarJUnit {
-	
-	
+
 	@Test
 	public void testRecargaExitosa() {
-		Maquina rubia= new Maquina("C001", "Pilsener", "Cerveza", 0.02, 8000);
-		
+
+		Maquina rubia = new Maquina("C001", "Pilsener", "Cerveza", 0.02, 8000);
+
 		boolean resultado = rubia.recargarCerveza(3000);
+
 		assertTrue(resultado);
 		assertEquals(3000, rubia.getCantidadActual(), 0.0001);
 	}
-	
-	
+
 	@Test
 	public void testRecargaFallida() {
-		Maquina negra= new Maquina("C002", "Club", "Cerveza fría", 0.02, 8000);
-		
-		negra.recargarCerveza(7000);
-		
-		boolean resultado = negra.recargarCerveza(1000);
-		assertTrue(resultado);
-		assertEquals(3000, negra.getCantidadActual(), 0.0001);
-	}
-	
-	
-	
 
+		Maquina negra = new Maquina("C002", "Club", "Cerveza fría", 0.02, 8000);
+
+		negra.recargarCerveza(7000);
+
+		boolean resultado = negra.recargarCerveza(1000);
+
+		assertFalse(resultado);
+		assertEquals(7000, negra.getCantidadActual(), 0.0001);
+	}
 }
